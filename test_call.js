36fbf224e8ad15bf20e4a7f33460dcf4d25f116a
thisisdisco <danil@thisisdisco.tech>
2018-09-10 00:40:02 +0300

    Update test_call.js

--- test_call.js
+++ test_call.js
@@ -2,15 +2,18 @@ const speechOptions = { pitch: 'high', volume: 'loud', rate: 'x-slow' };
 
 VoxEngine.addEventListener(AppEvents.CallAlerting, event => {
   const tones = [];
+  let hashedCount = 0;
   const { call } = event;
   call.answer();
-  call.say('Please, enter 2 numbers from your keyboard', Language.UK_ENGLISH_MALE, speechOptions);
+  call.say('Please, enter 2 numbers, separated by #', Language.UK_ENGLISH_MALE, speechOptions);
   call.handleTones(true);
   call.addEventListener(CallEvents.ToneReceived, e => {
-    tones.push(parseInt(e.tone, 10));
-    if (tones.length >= 2) {
-      const sum = tones.reduce((res, t) => res + t, 0);
+    const { tone } = e;
+    if (tone === '#') hashedCount++;
+    if (hashedCount >= 2) {
+      const sum = tones.join('').split('#').reduce((res, num) => res + parseInt(num, 10), 0);
       call.say(sum.toString(), Language.UK_ENGLISH_MALE, speechOptions);
     }
+    tones.push(e.tone);
   });
 });
